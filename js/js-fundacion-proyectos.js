f003e95bb9d7c00cdf89689887d6d28991bab2bc
TOROLOCOJHPR <jesusparra07hp@gmail.com>
2018-03-22 13:03:13 -0700

    -- centrar tarjetas de descripción de proyectos

--- js/js-fundacion-proyectos.js
+++ js/js-fundacion-proyectos.js
@@ -2,13 +2,20 @@ $(document).ready(function(){
 
     $('.card').click(function(){
         //$('.lightbox').css({"display":"block"});
-        $('.lightbox-proyectos').addClass('c-align-middle');
+        $('.lightbox-proyectos').addClass('lightbox-block');
         var d =$(this).attr('d');        
         $('#dt').html(d).show();
+
+        $('body').css({
+            'overflow': 'hidden',
+        });
     });
     $('.lightbox-proyectos').click(function(){
         //$(this).css({"display":"none"});
-        $(this).removeClass('c-align-middle');
+        $(this).removeClass('lightbox-block');
+        $('body').css({
+            'overflow': 'auto',
+        });
     });
     $("#dt").click(function(){
         return false;
