105396ceb7a3d6eb201e2926b8b749feee939f1c
TOROLOCOJHPR <jesusparra07hp@gmail.com>
2018-03-22 12:14:47 -0700

    -- rediseñar descripciones de formulario

--- mod/formulario/descBrazo.php
+++ mod/formulario/descBrazo.php
@@ -1,4 +1,4 @@
-<div class="container-fluid text-white">
+<!-- <div class="container-fluid text-white">
             <div class="row pt-2 pb-2">
                 <div class="col-12 col-sm-6">
                     <div class="card w-75 mx-auto">
@@ -23,4 +23,25 @@
                     </div>
                 </div>
             </div>
+        </div> -->
+
+
+<div class="row mx-0 pt-4"> 
+    <div class="col-12 col-sm-6 c-align-middle p-5 order-2 order-md-1">
+        <div class="d-block text-center">
+            <h3 class="">Descripción</h3>
+            <p>"Protésis mecanica destinada a niños y adolescentes en edad de 6 a 18 años"</p>
+            <p >"Protésis robotíca destinada a personas de 18 años en adelante"</p>
         </div>
+    </div>
+    <div class="col-12 col-sm-6 order-1 order-md-2">
+        <div class="card w-75 mx-auto">
+            <div class="w-100 c-img bg-primary">
+                <img class=" img-cover-center w-100 h-100" src="../imagenes/fundación/dos-protesis.jpg" alt="Colchón vittmat">
+            </div>
+            <div class="w-100  c-text text-white bg-verde-menu c-align-middle flex-column">
+                <p class="mb-0">Protésis Antebrazo</p>
+            </div>
+        </div>
+    </div>            
+</div>
--- mod/formulario/descColchon.php
+++ mod/formulario/descColchon.php
@@ -1,6 +1,9 @@
-<div class="row mx-0"> 
-    <div class="col-12 col-sm-6 c-align-middle p-5 order-2 order-md-1 text-center ">
-        <p>"Colchón vibrador que ayuda en la prevención de neumonía y ulceras en la piel, para personas postradas en cama por causa de alguna enfermedad o accidente."</p>
+<div class="row mx-0 pt-4"> 
+    <div class="col-12 col-sm-6 c-align-middle p-5 order-2 order-md-1">
+        <div class="block  text-center">
+            <h3>Descripción</h3>
+            <p>"Colchón vibrador que ayuda en la prevención de neumonía y ulceras en la piel, para personas postradas en cama por causa de alguna enfermedad o accidente."</p>
+        </div>
     </div>
     <div class="col-12 col-sm-6 order-1 order-md-2">
         <div class="card w-75 mx-auto">
